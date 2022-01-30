a3f3239d1c6034ce7980b0261af092c7893274b1
esteban_89_1 <esteban_89_1@hotmail.com>
2022-01-30 00:56:21 -0300

    Initial commit

---
+++ src/main/java/com/example/httpsapi/HttpsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.httpsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsApiApplication.class, args);
+	}
+
+}
